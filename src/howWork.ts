78451b7d6559b666e5184927f6c903f9c3099563
mr9 asif <asifalibd002@gmail.com>
2026-01-02 23:06:35 +0600

    fix

--- src/howWork.ts
+++ src/howWork.ts
@@ -2,9 +2,3 @@
 // then in html we can add js file with a folder like output casue where we will get all compile file there because typescript work ulimatly on js. it covert that in js
 // then we can create a tsconfig.json file using --- tsc --init to more controll.
 //  also for run we can use only tsc after setting our src and ouput. but we can use -w to watch live
-// it's so important for us
-// it's so important for us
-// it's so important for us
-// it's so important for us
-// it's so important for us
-// it's so important for us
